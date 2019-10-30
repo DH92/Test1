64fcdb6691c57295d8496e440cc9576e0895ed79
unknown <hoffmand@hfu-rz.hs-furtwangen.de>
2019-10-30 10:31:00 +0100

    Felix TicTacToe: CreateField

---
+++ Exercises/Exercise_3_Felix_UML/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Exercise_2_Felix_UML
+{
+    class Program
+    {
+
+        public static string [] ticTacToe = new string [16]{
+                " ","-","-","-\n",
+                "|","","","\n",
+                "|","","","\n",
+                "|","","","\n",
+
+
+        }; 
+     
+
+
+     public static string matchField;
+        static void Main(string[] args)
+        {
+          
+            
+            Console.WriteLine(CreateField());
+            Console.WriteLine("Player 1 your turn:");
+         string  playerInput = Convert.ToString(Console.ReadLine());
+        }
+
+
+
+          static string CreateField()
+        {
+
+            foreach (string a in ticTacToe)
+            {
+                matchField = matchField + a;
+            }
+
+            return matchField;
+        }
+    }
+
+    
+}
